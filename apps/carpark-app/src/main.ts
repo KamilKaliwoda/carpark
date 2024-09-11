fabafbe5a0dcc543a8dac30321afe6ea2fba80df
Kamil <kamiljacek0@gmail.com>
2024-09-11 03:33:54 +0200

    layout improvements

--- apps/carpark-app/src/main.ts
+++ apps/carpark-app/src/main.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { PostUpdate } from './postUpdate';
 import sql from 'mssql';
-import { Client } from 'pg';
+import { Client, Pool } from 'pg';
 import { db_config, client_config } from './config/config';
 import { Endpoints } from './endpoints';
 import cors from 'cors';
@@ -16,13 +16,9 @@ dotenv.config();
 
 export const app = express();
 
-app.use(
-  cors({
-    origin: ['https://www.carpark.site', 'https://carpark.site', 'https://www.carpark.site.', 'https://carpark.site.']
-  }),
-);
+app.use(cors());
 
-const sql = new Client(client_config);
+const sql = new Pool(client_config);
 
 sql.connect(function(err) {
   if (err) throw err;
